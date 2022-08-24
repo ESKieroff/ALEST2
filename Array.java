0cac38d1fa1baf662a877de82a61b967a65cb882
ESKieroff <Rodrigo.Rosa81@edu.pucrs.br>
2022-08-24 23:17:55 +0000

    vai

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Array {
+
+    private ArrayList<Integer> lista;
+
+
+    public static void main(String[]args){
+
+
+    }
+
+    private int add()
+
+}
